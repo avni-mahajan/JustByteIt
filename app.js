5d38baa40f95ec2aa2428a9485f1ed646ad352f6
harsheeka <yoyoharshikaa@gmail.com>
2024-12-09 16:37:05 +0530

    updated app.js with now importing react from npm

--- app.js
+++ app.js
@@ -1,13 +1,5 @@
-{/* <div id="parent">
-    <div id="child1">
-        <h1>Heading 1 of child 1</h1>
-        <h2>Heading 2 of child 1</h2>
-    </div>
-    <div id="child2">
-        <h1>Heading 1 of child 2</h1>
-        <h2>Heading 2 of child 2</h2>
-    </div>
-</div> */}
+import React from "react"
+import ReactDOM from "react-dom/client"
 
 
 const loop = React.createElement("div",{id: "parent"},[
