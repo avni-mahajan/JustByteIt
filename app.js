d507c4ee42c3f1b20482eef493acef35474ccf9d
harsheeka <yoyoharshikaa@gmail.com>
2024-12-09 23:26:04 +0530

    added about react components and introduced JSX

--- app.js
+++ app.js
@@ -2,12 +2,33 @@ import React from "react"
 import ReactDOM from "react-dom/client"
 
 // writing the code using react, we are directly creating the react element which will be further converted to  html code
-const heading = React.createElement("h1",{id:"heading"},"Hello");
+// const heading = React.createElement("h1",{id:"heading"},"Hello");
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(heading);
+// root.render(heading);
 
 // we have an extra step here and the code is written in jsx which is first converted to react elemet amd then the process continues !!
-const headi = <h1 id =" head">Hellooooooo</h1>;
-root.render(headi);
+const ele = <h1>This is an element</h1>;
+// root.render(headi);
+
+
+//this is a react component
+// const HeadingComponent = () => {
+//     return <h1> Hello from the component </h1>;     
+// }
+
+//another way to write react compenent
+const HeadingComponent = () => <h1>Hello from heading</h1> ;
+
+//This is called as component composition
+const Container = () => (
+    <div>
+        {ele}   
+        <HeadingComponent/>
+        <h2>Hello from container</h2>
+    </div>
+);
+
+root.render(<Container/>);
+
 
