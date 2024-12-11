5654ff2650be1e1f00746c83240030496a9c2f87
harsheeka <yoyoharshikaa@gmail.com>
2024-12-11 14:59:26 +0530

    added hover effect for restruent cards

--- app.js
+++ app.js
@@ -20,11 +20,50 @@ const HeaderComponent = () => {
     </div>
 )}
 
+const ResCard = () =>{
+    return (
+        <div className="res-card">
+            <img className="res-img" src="https://images.jdmagicbox.com/v2/comp/delhi/x6/011pxx11.xx11.230101185944.w7x6/catalogue/kfc-vivekanand-puri-delhi-american-restaurants-rut6vpclea.jpg" />
+            <h3>KFC</h3>
+            <h3>Burgers,Fast Food,Rolls</h3>
+            <h3>4.4 Stars</h3>
+        </div>
+    )
+}
+
+const BodyComponent = () => {
+    return (
+        <div className="body">
+            <div className="search"> 
+                 Search
+            </div>
+            <div className = "res-container">
+                <ResCard/>
+                <ResCard/>
+                <ResCard/>
+                <ResCard/>
+                <ResCard/>
+                <ResCard/>
+                <ResCard/>
+                <ResCard/>
+                <ResCard/>
+                <ResCard/>
+                <ResCard/>
+                <ResCard/>
+                <ResCard/>
+                <ResCard/>
+                <ResCard/>
+            </div>
+        </div>
+    )
+}
+
 
 const AppLayout = () => {
     return (
         <div className = "app">
             <HeaderComponent/>
+            <BodyComponent/>
         </div>
     )
 }
