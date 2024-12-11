79c1902f6fa7ffa328a6ce6d63cf54a333870a35
harsheeka <yoyoharshikaa@gmail.com>
2024-12-11 13:55:29 +0530

    added header component for food webapp

--- app.js
+++ app.js
@@ -1,34 +1,34 @@
 import React from "react"
 import ReactDOM from "react-dom/client"
 
-// writing the code using react, we are directly creating the react element which will be further converted to  html code
-// const heading = React.createElement("h1",{id:"heading"},"Hello");
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-// root.render(heading);
-
-// we have an extra step here and the code is written in jsx which is first converted to react elemet amd then the process continues !!
-const ele = <h1>This is an element</h1>;
-// root.render(headi);
-
-
-//this is a react component
-// const HeadingComponent = () => {
-//     return <h1> Hello from the component </h1>;     
-// }
-
-//another way to write react compenent
-const HeadingComponent = () => <h1>Hello from heading</h1> ;
-
-//This is called as component composition
-const Container = () => (
-    <div>
-        {ele}   
-        <HeadingComponent/>
-        <h2>Hello from container</h2>
+const HeaderComponent = () => {
+    return(
+    <div className = "header">
+        <div className = "logo-container">
+            <img className = "logo" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQzTo7Sb8iYLjFeTf-I8Rnz7qnVpPzr6EvSUQ&s"/>
+        </div>
+        <div className="nav-bar">
+            <ul>
+                <li>Home</li>
+                <li>About Us</li>
+                <li>Contact Us</li>
+                <li>Cart</li>
+            </ul>
+        </div>
     </div>
-);
+)}
 
-root.render(<Container/>);
+
+const AppLayout = () => {
+    return (
+        <div className = "app">
+            <HeaderComponent/>
+        </div>
+    )
+}
+
+root.render(<AppLayout/>);
 
 
