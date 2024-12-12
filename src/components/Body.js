77a06da741da332bcefd158ff9af55dad2f86d6c
harsheeka <yoyoharshikaa@gmail.com>
2024-12-13 01:22:39 +0530

    added cors proxy

--- src/components/Body.js
+++ src/components/Body.js
@@ -10,7 +10,7 @@ const Body = () => {
 
     const fetchData = async() =>{   //fetch data from an api !!!!!
 
-        const data = await fetch("https://www.swiggy.com/dapi/restaurants/list/v5?lat=30.7333148&lng=76.7794179&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING");    //we use asynch and await to resolve promise
+        const data = await fetch("https://proxy.cors.sh/https://www.swiggy.com/dapi/restaurants/list/v5?lat=30.7333148&lng=76.7794179&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING");    //we use asynch and await to resolve promise
         const json = await data.json();   //convert the data fetched to json
 
         setlisRes(json?.data?.cards[4]?.card?.card?.gridElements?.infoWithStyle?.restaurants); //to update the new  rendered data on UI !!!!!!!!!
