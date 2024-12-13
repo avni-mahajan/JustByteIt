a606e02c35ba0efb20d9b83c32c96f7c72835d18
harsheeka <yoyoharshikaa@gmail.com>
2024-12-13 18:17:16 +0530

    added a const for kitty error image

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,2 +1,3 @@
 export const LOGO_CDN = "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/" ;
 export const LOGO_URL = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQzTo7Sb8iYLjFeTf-I8Rnz7qnVpPzr6EvSUQ&s";
+export const LOGO_ERR = "https://img.freepik.com/free-vector/404-error-with-cute-animal-concept-illustration_114360-1919.jpg?t=st=1734093366~exp=1734096966~hmac=ae80956666bf8881a97faeb66d0d4b8bf3f7f6a6b3e89ca567cabee07a5bd355&w=740" ;
