de419b4c057a2331e5307652131f3387233b78fb
harsheeka <yoyoharshikaa@gmail.com>
2024-12-15 22:54:06 +0530

    minor changes

--- src/components/About.js
+++ src/components/About.js
@@ -2,6 +2,7 @@ import UserClass from "./userClass";
 const About = () => {
     return (
         <div className="aboutus">
+            <h1>This is ABout </h1>
             <UserClass name = {"Harshika"} location = {"Punjab"}/>
         </div>
     )
