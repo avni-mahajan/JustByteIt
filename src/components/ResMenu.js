05cf0264ffca9ad22faf3123bc22641b6c5ee98a
harsheeka <yoyoharshikaa@gmail.com>
2024-12-20 23:27:42 +0530

    removed resmenu UI

--- src/components/ResMenu.js
+++ src/components/ResMenu.js
@@ -22,22 +22,7 @@ const ResMenu = () => {
 
   return (
     <div className="menu">
-      <h1 className="font-extrabold text-center text-4xl">{restaurantName}</h1>
-      <div className = "flex flex-wrap">
-        {menuItems?.map((item, index) => {
-          const { name, imageId } = item?.card?.info || {};
-          return (
-            <div key={index} className="w-60 h-3/4 m-5 p-2 bg-yellow-50 rounded-xl hover:scale-95 hover:cursor-pointer">
-              <h2 className="font-bold">{name || "Menu Item Unavailable"}</h2>
-              <img
-                className="w-60 h-2/3 rounded-xl"
-                src={`https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,h_600/${imageId}`}
-                alt={name}
-              />
-            </div>
-          );
-        })}
-      </div>
+      
     </div>
   );
 };
