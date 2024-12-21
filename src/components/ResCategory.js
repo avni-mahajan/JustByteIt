1d38b5fa3828121548f34591668de2f3a343063e
harsheeka <yoyoharshikaa@gmail.com>
2024-12-22 00:05:33 +0530

    now the accordian opens and closes!!

--- src/components/ResCategory.js
+++ src/components/ResCategory.js
@@ -1,14 +1,20 @@
+import { useState } from "react";
 import ItemList from "./ItemList";
 
 const Rescategory = ({data}) => {
 
+    const [showItems,setshowItems] = useState(false);
+    const handleClick = () => {
+        setshowItems(!showItems); 
+    };
+
     return <div className="w-6/12 bg-gray-50 shadow-lg p-4 mx-auto ">
-        <div className=" flex justify-between">
+        <div className=" flex justify-between cursor-pointer" onClick={handleClick}>
             <span className="font-bold">{data.title} ({data.itemCards.length}) </span>
             <span> icon </span>
         </div>
         <div>
-            <ItemList items = {data?.itemCards}/>
+            {showItems && <ItemList items = {data?.itemCards}/>}
         </div>
     </div>
 };
