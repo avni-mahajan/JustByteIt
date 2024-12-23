7f21268bac9bc29f821ce7486fff53718604ec46
harsheeka <yoyoharshikaa@gmail.com>
2024-12-23 21:54:27 +0530

    carousel compoennt

--- src/components/Carousel.js
+++ src/components/Carousel.js
@@ -1,17 +1,22 @@
 import { carouselImg } from "../utils/constants";
 
-const Carousel = ({carouselRes}) => {
-    return (
-        <div key={carouselRes?.id}  className="flex" >
-            {carouselRes.map((restaurant) => (
-            <div className="">
-                <img src= {carouselImg + restaurant?.imageId}/>
-            </div>
+const Carousel = ({ carouselRes }) => {
+  return (
+    <div className="relative">
+      {/* Scrollable Container */}
+      <div className="flex overflow-x-auto scrollbar scrollbar-thumb-yellow-400 scrollbar-track-yellow-100 py-4">
+        {carouselRes.map((restaurant) => (
+          <div key={restaurant?.id} className="mx-2 flex-shrink-0">
+            <img
+              src={carouselImg + restaurant?.imageId}
+              alt={restaurant?.name}
+              className="w-40 h-auto"
+            />
+          </div>
         ))}
-
-        </div>
-        
-    );
+      </div>
+    </div>
+  );
 };
 
 export default Carousel;
