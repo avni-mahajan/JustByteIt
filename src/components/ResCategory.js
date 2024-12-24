07772fdfef84c5f4271aeb53379f0ac846330628
harsheeka <yoyoharshikaa@gmail.com>
2024-12-25 02:17:57 +0530

    added the state managment for accordians and used svg for arrows for accordian

--- src/components/ResCategory.js
+++ src/components/ResCategory.js
@@ -1,20 +1,42 @@
+import { useState } from "react";
 import ItemList from "./ItemList";
 
-const Rescategory = ({ data, showItems, setshowItems }) => {
+const Rescategory = ({ data }) => {
+  const [showItems, setshowItems] = useState(false);
   const handleClick = () => {
     setshowItems(!showItems);
   };
 
   return (
-    <div className="w-6/12 bg-gray-50 shadow-lg p-4 mx-auto ">
+    <div className="p-4 mx-auto">
       <div
-        className=" flex justify-between cursor-pointer"
+        className="flex justify-between cursor-pointer"
         onClick={handleClick}
       >
         <span className="font-bold">
-          {data.title} ({data.itemCards.length}){" "}
+          {data.title} ({data.itemCards.length})
         </span>
-        <span> icon </span>
+        <div>
+          {showItems ? (
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="text-pink-400 w-8 h-8"
+              fill="currentColor"
+              viewBox="0 0 16 16"
+            >
+              <path d="M4.293 5.293a1 1 0 0 1 1.414 0L8 7.586l2.293-2.293a1 1 0 0 1 1.414 1.414l-3 3a1 1 0 0 1-1.414 0l-3-3a1 1 0 0 1 0-1.414z" />
+            </svg>
+          ) : (
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="text-pink-400 w-8 h-8"
+              fill="currentColor"
+              viewBox="0 0 16 16"
+            >
+              <path d="M4.293 10.707a1 1 0 0 1 1.414 0L8 8.414l2.293 2.293a1 1 0 0 1 1.414-1.414l-3-3a1 1 0 0 1-1.414 0l-3 3a1 1 0 0 1 0 1.414z" />
+            </svg>
+          )}
+        </div>
       </div>
       <div>{showItems && <ItemList items={data?.itemCards} />}</div>
     </div>
