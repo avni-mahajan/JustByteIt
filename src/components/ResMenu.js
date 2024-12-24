854adfc4294bfd0bb1520cdbdb33f6e19ac99301
harsheeka <yoyoharshikaa@gmail.com>
2024-12-25 02:18:22 +0530

    removed state managemnt and moved it back to res category

--- src/components/ResMenu.js
+++ src/components/ResMenu.js
@@ -2,12 +2,11 @@ import Shimmer from "./Shimmer";
 import { useParams } from "react-router-dom";
 import useResMenu from "../utils/useResMenu";
 import ResCategory from "./ResCategory";
-import { useState } from "react";
+import starRating from "../utils/images/star-rating.png";
 
 const ResMenu = () => {
   const { resID } = useParams(); // to fetch restaurant ID dynamically from the route
   const resInfo = useResMenu(resID);
-  const [showIndex, setShowIndex] = useState(0);
 
   if (resInfo === null) {
     return <Shimmer />;
@@ -21,27 +20,51 @@ const ResMenu = () => {
         "type.googleapis.com/swiggy.presentation.food.v2.ItemCategory"
     );
 
-  console.log(categories);
-
-  const restaurantName =
-    resInfo?.cards?.[2]?.card?.card?.info?.name ||
-    "Restaurant Name Unavailable";
-
-  const menuItems = resInfo?.cards
-    ?.find((card) => card?.groupedCard)
-    ?.groupedCard?.cardGroupMap?.REGULAR?.cards // Access REGULAR cards
-    ?.map((card) => card?.card?.card?.itemCards || []);
+  const {
+    name,
+    areaName,
+    costForTwoMessage,
+    cuisines,
+    avgRating,
+    totalRatingsString,
+    sla,
+  } = resInfo?.cards?.[2]?.card?.card?.info;
 
   return (
-    <div className="text-center">
-      <h1 className="font-bold text-2xl my-10">{restaurantName}</h1>
+    <div className="mx-96">
+      <div className=" p-5 rounded-[40px] my-8 bg-gradient-to-t from-gray-200 to-white">
+        <h1 className="font-extrabold text-2xl mb-5 ">{name}</h1>
+        <div className=" border-2 border-gray-200 rounded-[27px]  p-4 bg-white">
+          <div className="flex ml-2">
+            <ul className="flex ">
+              <li>
+                <div className="flex">
+                  <img className="w-5 h-5 mt-0.5 mr-2" src={starRating} />
+                  <h4 className="font-bold mr-2">
+                    {avgRating}({totalRatingsString})
+                  </h4>
+                </div>
+              </li>
+              <li className="list-disc ml-4">
+                <h4 className="font-bold">{costForTwoMessage}</h4>
+              </li>
+            </ul>
+          </div>
+          <h4 className="truncate ml-3 text-pink-500 font-bold text-sm my-4">
+            {cuisines.join(", ")}
+          </h4>
+          <div className="flex">
+            <h4 className="truncate ml-3 font-bold text-sm">Outlet</h4>
+            <h4 className="truncate ml-3 text-gray-600 text-sm">{areaName}</h4>
+          </div>
+          <h4 className="truncate ml-3 text-sm font-bold">{sla?.slaString}</h4>
+        </div>
+      </div>
       <div>
         {categories.map((category, index) => (
           <ResCategory
             key={category?.card?.card.title}
             data={category?.card?.card}
-            showItems={index === showIndex ? true : false}
-            setshowItems={() => setShowIndex(index)}
           />
         ))}
       </div>
