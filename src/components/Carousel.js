e4694d7c5e3169d1ed73de070b783558229846d4
harsheeka <yoyoharshikaa@gmail.com>
2024-12-25 02:15:36 +0530

    removing uncessary comments

--- src/components/Carousel.js
+++ src/components/Carousel.js
@@ -3,8 +3,7 @@ import { carouselImg } from "../utils/constants";
 const Carousel = ({ carouselRes }) => {
   return (
     <div className="relative">
-      {/* Scrollable Container */}
-      <div className="flex overflow-x-auto scrollbar-none scrollbar-thumb-pink-400 scrollbar-track-pink-200 py-4">
+      <div className="flex overflow-x-auto scrollbar-none py-4">
         {carouselRes.map((restaurant) => (
           <div key={restaurant?.id} className="mx-2 flex-shrink-0">
             <img
