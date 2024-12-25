5c764f340a45f00bb8a18982481afe9d0efc0ac3
harsheeka <yoyoharshikaa@gmail.com>
2024-12-25 17:57:29 +0530

    updated grocery page

--- src/components/Grocery.js
+++ src/components/Grocery.js
@@ -1,7 +1,13 @@
+import grocery from "../utils/images/grocery.jpg"
 const Grocery = () => {
-    return (
-        <h1>This would be our grocery component that will have a lot of child component</h1>
-    );
+  return (
+    <div className="">
+      <h1 className="font-extrabold text-4xl mt-10 mr-3 text-pink-600 ml-[400px]">
+        This section will be expanded in future! 
+      </h1>
+      <img src={grocery} className="w-[600px] mx-[420px]" />
+    </div>
+  );
 };
 
 export default Grocery;
