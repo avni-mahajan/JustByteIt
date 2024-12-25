2b3a213b5198d3051adf4ef10490d8997f6ad1fd
harsheeka <yoyoharshikaa@gmail.com>
2024-12-25 19:02:18 +0530

    minor

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,7 +1,7 @@
 export const LOGO_CDN = "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/" ;
 export const LOGO_URL = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQzTo7Sb8iYLjFeTf-I8Rnz7qnVpPzr6EvSUQ&s";
 export const LOGO_ERR = "https://img.freepik.com/free-vector/404-error-with-cute-animal-concept-illustration_114360-1919.jpg?t=st=1734093366~exp=1734096966~hmac=ae80956666bf8881a97faeb66d0d4b8bf3f7f6a6b3e89ca567cabee07a5bd355&w=740" ;
-export const MENU_A = "https://proxy.cors.sh/https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=30.7333148&lng=76.7794179&restaurantId=";
+export const MENU_A = "https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=30.7333148&lng=76.7794179&restaurantId=";
 export const MENU_B = "&catalog_qa=undefined&submitAction=ENTER";
 export const carouselImg = "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_288,h_360/";
 export const API_CALL = "https://www.swiggy.com/dapi/restaurants/list/v5?lat=30.7333148&lng=76.7794179&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING";
