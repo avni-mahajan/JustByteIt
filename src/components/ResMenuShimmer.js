e6496f2a132b4c7a048cc643aabddfd47784baaf
harsheeka <yoyoharshikaa@gmail.com>
2025-07-28 23:14:09 +0530

    update in ui

--- src/components/ResMenuShimmer.js
+++ src/components/ResMenuShimmer.js
@@ -2,7 +2,6 @@ const ResMenuShimmer = () => {
   return (
     <div className="mx-96">
     <div className="px-4 sm:px-10 md:px-20 py-6 animate-pulse">
-      {/* Restaurant Header Skeleton */}
       <div className="bg-pink-100 rounded-xl p-6 mb-6 shadow-sm">
         <div className="h-6 w-1/3 bg-pink-300 rounded mb-2"></div>
         <div className="flex items-center space-x-4 mb-2">
@@ -14,15 +13,13 @@ const ResMenuShimmer = () => {
         <div className="w-1/5 h-4 bg-pink-300 rounded" />
       </div>
 
-      {/* Category title */}
       <div className="w-1/3 h-5 bg-pink-300 rounded mb-4" />
 
-      {/* Repeating shimmer items */}
       {Array(5)
         .fill("")
         .map((_, idx) => (
           <div key={idx} className="flex justify-between items-start py-6 border-b border-pink-200">
-            {/* Text content */}
+
             <div className="w-8/12 pr-4">
               <div className="flex items-center space-x-2 mb-2">
                 <div className="w-4 h-4 bg-pink-300 rounded-full" />
@@ -38,7 +35,6 @@ const ResMenuShimmer = () => {
               <div className="w-3/4 h-3 bg-pink-300 rounded" />
             </div>
 
-            {/* Image shimmer */}
             <div className="w-28 h-28 bg-pink-300 rounded-xl relative">
               <div className="absolute bottom-0 left-1/2 -translate-x-1/2 w-20 h-8 bg-white border border-pink-300 rounded-full" />
             </div>
--- src/components/Shimmer.js
+++ src/components/Shimmer.js
@@ -1,42 +1,31 @@
 const Shimmer = () => {
   return (
-    <div className="flex flex-wrap mx-56 my-5">
-      <div className="w-56 h-3/4  p-2 m-5 rounded-xl bg-pink-100 ">
-        <div className="relative w-full h-48 sm:w-72 lg:h-40 lg:w-64"></div>
+    <div className="mx-56 my-10">
+    <div className="p-8 animate-pulse">
+      <div className="flex gap-6 overflow-x-auto mb-10">
+        {Array(6).fill(0).map((_, i) => (
+          <div key={i} className="flex flex-col items-center">
+            <div className="w-20 h-20 rounded-full bg-pink-100 mb-2"></div>
+            <div className="w-16 h-4 bg-pink-200 rounded"></div>
+          </div>
+        ))}
       </div>
-      <div className="w-56 h-3/4  p-2 m-5 rounded-xl bg-pink-100 ">
-        <div className="relative w-full h-48 sm:w-72 lg:h-40 lg:w-64"></div>
+
+      <div className="h-6 w-72 bg-pink-200 rounded mb-6"></div>
+
+      <div className="flex flex-wrap gap-6">
+        {Array(8).fill(0).map((_, i) => (
+          <div
+            key={i}
+            className="w-64 p-3 rounded-xl bg-pink-100 shadow-sm"
+          >
+            <div className="h-36 w-full bg-pink-200 rounded mb-3"></div>
+            <div className="h-4 bg-pink-200 rounded w-3/4 mb-2"></div>
+            <div className="h-4 bg-pink-200 rounded w-1/2 mb-1"></div>
+          </div>
+        ))}
       </div>
-      <div className="w-56 h-3/4  p-2 m-5 rounded-xl bg-pink-100 ">
-        <div className="relative w-full h-48 sm:w-72 lg:h-40 lg:w-64"></div>
-      </div>
-      <div className="w-56 h-3/4  p-2 m-5 rounded-xl bg-pink-100 ">
-        <div className="relative w-full h-48 sm:w-72 lg:h-40 lg:w-64"></div>
-      </div>
-      <div className="w-56 h-3/4  p-2 m-5 rounded-xl bg-pink-100 ">
-        <div className="relative w-full h-48 sm:w-72 lg:h-40 lg:w-64"></div>
-      </div>
-      <div className="w-56 h-3/4  p-2 m-5 rounded-xl bg-pink-100 ">
-        <div className="relative w-full h-48 sm:w-72 lg:h-40 lg:w-64"></div>
-      </div>
-      <div className="w-56 h-3/4  p-2 m-5 rounded-xl bg-pink-100 ">
-        <div className="relative w-full h-48 sm:w-72 lg:h-40 lg:w-64"></div>
-      </div>
-      <div className="w-56 h-3/4  p-2 m-5 rounded-xl bg-pink-100 ">
-        <div className="relative w-full h-48 sm:w-72 lg:h-40 lg:w-64"></div>
-      </div>
-      <div className="w-56 h-3/4  p-2 m-5 rounded-xl bg-pink-100 ">
-        <div className="relative w-full h-48 sm:w-72 lg:h-40 lg:w-64"></div>
-      </div>
-      <div className="w-56 h-3/4  p-2 m-5 rounded-xl bg-pink-100 ">
-        <div className="relative w-full h-48 sm:w-72 lg:h-40 lg:w-64"></div>
-      </div>
-      <div className="w-56 h-3/4  p-2 m-5 rounded-xl bg-pink-100 ">
-        <div className="relative w-full h-48 sm:w-72 lg:h-40 lg:w-64"></div>
-      </div>
-      <div className="w-56 h-3/4  p-2 m-5 rounded-xl bg-pink-100 ">
-        <div className="relative w-full h-48 sm:w-72 lg:h-40 lg:w-64"></div>
-      </div>
+    </div>
     </div>
   );
 };
